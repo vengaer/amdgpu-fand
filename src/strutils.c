1a6d38839cb26b8ac00cec487ab7cf9d34f89a47
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-11-02 18:35:30 +0100

    Add strscpy and replace_char

--- src/strutils.c
+++ src/strutils.c
@@ -1,17 +1,40 @@
 #include "strutils.h"
 
+#include <stddef.h>
 #include <string.h>
 
-int strscat(char *restrict dst, char const *restrict src, size_t count) {
+int strscpy(char *restrict dst, char const *restrict src, size_t count) {
     size_t const src_len = strlen(src);
-    size_t const dst_len = strlen(dst);
 
-    strncat(dst, src, count - dst_len);
+    strncpy(dst, src, count);
     dst[count - 1] = '\0';
 
-    if(src_len >= count - dst_len) {
+    if(src_len >= count) {
         return -E2BIG;
     }
 
     return src_len;
+}
+
+int strscat(char *restrict dst, char const *restrict src, size_t count) {
+    size_t const src_len = strlen(src);
+    size_t const dst_len = strlen(dst);
+    size_t const rem = count - dst_len;
+
+    strncat(dst, src, rem);
+    dst[count - 1] = '\0';
+
+    if(src_len >= rem) {
+        return -E2BIG;
+    }
+
+    return src_len;
+}
+
+void replace_char(char *string, char from, char to) {
+    for(size_t i = 0; i < strlen(string); i++) {
+        if(string[i] == from) {
+            string[i] = to;
+        }
+    }
 }
--- src/strutils.h
+++ src/strutils.h
@@ -7,6 +7,9 @@
 #define E2BIG 7
 #endif
 
+int strscpy(char *restrict dst, char const *restrict src, size_t count);
 int strscat(char *restrict dst, char const *restrict src, size_t count);
+
+void replace_char(char *string, char from, char to);
 
 #endif
