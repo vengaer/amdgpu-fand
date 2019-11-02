2458afcf12c568776112dadaa8ee172a70ea312d
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-11-02 22:48:43 +0100

    Move matrix type

--- src/config.h
+++ src/config.h
@@ -1,17 +1,14 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include "matrix.h"
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
 #define FANCTL_CONFIG "/etc/amdgpu-fanctl.conf"
 #define COMMENT_CHAR '#'
-
-#define MATRIX_COLS 2
-#define MATRIX_ROWS 16
-
-typedef uint8_t matrix[MATRIX_ROWS][MATRIX_COLS];
 
 bool parse_config(char const *restrict path, char *restrict hwmon, size_t hwmon_count, uint8_t *interval, matrix m, uint8_t *matrix_rows);
 
