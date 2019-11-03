534e744a7718aab49699dc2f7df0e6db6952e23e
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-11-03 11:34:21 +0100

    Add cosine interpolation

--- src/interpolation.h
+++ src/interpolation.h
@@ -4,12 +4,16 @@
 #include <math.h>
 #include <stdint.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846 /* pi */
+#endif
+
 
 /*
  * Linear interpolation
  */
 static inline uint8_t lerp_uint8(uint8_t a, uint8_t b, double x) {
-    return (uint8_t)(round(a + (b - a) * x));
+    return (uint8_t)round(a + (b - a) * x);
 }
 
 /*
@@ -23,6 +27,11 @@ static inline uint8_t lerp_uint8(uint8_t a, uint8_t b, double x) {
  */
 static inline uint8_t inverse_lerp_uint8(uint8_t a, uint8_t b, uint8_t x) {
     return (uint8_t)round(100.0 * (double)(x - a) / (double)(b - a));
+}
+
+static inline uint8_t consine_interpolate_uint8(uint8_t a, uint8_t b, double x) {
+    double xx = (1.0 - cos(x * M_PI)) / 2.0;
+    return lerp_uint8(a, b, xx);
 }
 
 #endif
