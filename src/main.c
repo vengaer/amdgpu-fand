bc32f5f7400ee3463622fcc7c837e231646701ac
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-11-05 20:25:08 +0100

    Use argp for argument parsing

--- src/main.c
+++ src/main.c
@@ -13,17 +13,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <regex.h>
+#include <argp.h>
 #include <signal.h>
 
 uint8_t log_level = 0;
 bool volatile daemon_alive = true;
-
-static regex_t hwmon_input_rgx, interval_input_rgx, config_input_rgx;
-static bool hwmon_passed = false, interval_passed = false;
-static char const *hwmon;
-static uint8_t update_interval;
-static char const *config = FANCTL_CONFIG;
 
 void signal_handler(int code) {
     if(code == SIGINT || code == SIGTERM) {
@@ -34,211 +28,162 @@ void signal_handler(int code) {
     }
 }
 
-static inline void increment_log_level(void) {
-    ++log_level;
-    if(log_level > MAX_LOG_LVL) {
+static inline void set_log_level(uint8_t verbose) {
+    if(verbose > MAX_LOG_LVL) {
         fprintf(stderr, "Warning, max log level is %d\n", MAX_LOG_LVL);
-        log_level = MAX_LOG_LVL;
+        verbose = MAX_LOG_LVL;
     }
+    log_level = verbose;
 }
 
-static void print_usage(void) {
-    fprintf(stderr,
-        "Usage: amdgpu-fan [option]...\n"
-        "Options:\n"
-        "    -v, --verbose                       Echo actions to stdout. Can be passed twice to set log level to 2\n"
-        "    -f FILE, --hwmon=FILE               Specify hwmon file, default is hwmon1\n"
-        "    -i INTERVAL, --interval=INTERVAL    Specify the interval with which to update fan speed, in seconds. The value must be in the interval 1...255.\n"
-        "                                        Default is read from %s\n"
-        "    -c FILE, --config=FILE              Override config location\n"
-        "    -h, --help                          Print help message\n", FANCTL_CONFIG);
-}
-
-static bool compile_regexps(void) {
-    int reti;
-    reti = regcomp(&hwmon_input_rgx, "^--hwmon=.+", REG_EXTENDED);
-    if(reti) {
-        fprintf(stderr, "Failed to compile hwmon input regex\n");
-        return false;
-    }
-    reti = regcomp(&interval_input_rgx, "^--interval=[0-9]+", REG_EXTENDED);
-    if(reti) {
-        fprintf(stderr, "Failed to compile interval input regex\n");
-        return false;
-    }
-    reti = regcomp(&config_input_rgx, "^--config=.+", REG_EXTENDED);
-    if(reti) {
-        fprintf(stderr, "Failed to compile config input regex\n");
-        return false;
-    }
-    return true;
-}
-
-static uint8_t handle_multi_switch(char const *switches) {
-    size_t const len = strlen(switches);
-    for(size_t i = 1; i < len; i++) {
-        if(switches[i] == 'v') {
-            increment_log_level();
-        }
-        else if(switches[i] == 'h') {
-            print_usage();
-            return 2;
-        }
-        else {
-            fprintf(stderr, "Unknown switch sequence %s\n", switches);
-            return 1;
-        }
-    }
-    return 0;
-}
-
-static bool set_interval(char const *interval, uint8_t *out_interval) {
-    if(!interval) {
-        fprintf(stderr, "Interval is empty\n");
-        return false;
-    }
+static bool uint8_from_chars(char const *str, uint8_t *value) {
     char *endptr;
-    long const ivl = strtol(interval, &endptr, 10);
+    long const v = strtol(str, &endptr, 10);
     if(*endptr) {
-        fprintf(stderr, "%s contains non-digit characters\n", interval);
+        fprintf(stderr, "%s contains non-digit characters\n", str);
         return false;
     }
-    if(ivl < 1 || ivl > 255) {
-        fprintf(stderr, "%ld is not in the interval 1...255\n", ivl);
+    if(v <= 0 || v > UINT8_MAX) {
+        fprintf(stderr, "%ld is not in the interval 1...%u\n", v, UINT8_MAX);
         return false;
     }
-    *out_interval = ivl;
+    *value = (uint8_t)v;
     return true;
 }
 
-static int parse_arguments(int argc, char** argv) {
-    for(int i = 1; i < argc; i++) {
-        if(strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
-            increment_log_level();
-        }
-        else if(strcmp(argv[i], "-h") == 0|| strcmp(argv[i], "--help") == 0) {
-            print_usage();
-            return 0;
-        }
-        else if(strcmp(argv[i], "-f") == 0) {
-            if(i + 1 >= argc) {
-                fprintf(stderr, "No argument supplied for -f\n");
-                return 1;
-            }
-            hwmon_passed = true;
-            hwmon = argv[++i];
-        }
-        else if(strcmp(argv[i], "-i") == 0) {
-            if(i + 1 >= argc) {
-                fprintf(stderr, "No argument supplied for -i\n");
-                return 1;
-            }
-            if(!set_interval(argv[++i], &update_interval)) {
-                return 1;
-            }
-            interval_passed = true;
-        }
-        else if(strcmp(argv[i], "-c") == 0) {
-            if(i + 1 >= argc) {
-                fprintf(stderr, "No argument supplied for -c\n");
-                return 1;
-            }
-            config = argv[++i];
-        }
-        else if(regexec(&hwmon_input_rgx, argv[i], 0, NULL, 0) == 0) {
-            hwmon = strchr(argv[i], '=') + 1;
-            hwmon_passed = true;
-        }
-        else if(regexec(&interval_input_rgx, argv[i], 0, NULL, 0) == 0) {
-            if(!set_interval(strchr(argv[i], '=') + 1, &update_interval)) {
-                return 1;
-            }
-            interval_passed = true;
-        }
-        else if(regexec(&config_input_rgx, argv[i], 0, NULL, 0) == 0) {
-            config = strchr(argv[i], '=') + 1;
-        }
-        else if(strlen(argv[i]) > 2 && argv[i][0] == '-' && argv[i][1] != '-') {
-            uint8_t const rv = handle_multi_switch(argv[i]);
-            if(rv != 0) {
-                return rv % 2;
-            }
-        }
-        else {
-            fprintf(stderr, "Unknown switch %s\n", argv[i]);
-            print_usage();
-            return 1;
-        }
+static bool generate_hwmon_path(char *restrict dst, char const *restrict hwmon, size_t count) {
+    /* Generate hwmon path */
+    if(strscat(dst, HWMON_DIR, count) < 0) {
+        fprintf(stderr, "hwmon dir overflows the path buffer\n");
+        return false;
     }
+
+    if(strscat(dst, hwmon, count) < 0) {
+        fprintf(stderr, "%s would overflow the buffer when appended to %s\n", hwmon, dst);
+        return false;
+    }
+
+    return true;
+}
+
+char const *argp_program_version = "amdgpu-fanctl 1.0";
+char const *argp_program_bug_address = "<vilhelm.engstrom@tuta.io";
+
+static char doc[] = "amdgpu-fanctl -- A daemon controlling the fan speed on AMD Radeon GPUs";
+static char args_doc[] = "";
+
+static struct argp_option options[] = {
+    {"verbose",  'v', 0,          0, "Echo actions to standard out. May be repeated up to 2 times to set log level", 0 },
+    {"hwmon",    'f', "FILE",     0, "Specify the hardware hwmon", 0 },
+    {"interval", 'i', "INTERVAL", 0, 
+     "Specify the interval, in seconds, with which to update fan speed. The value must be in the interval 1...255", 0 },
+    {"config",   'c', "FILE",     0, "Override config path", 0 },
+    { 0 }
+};
+
+struct arguments {
+    char *hwmon;
+    char *config;
+    uint8_t verbose;
+    uint8_t interval;
+    bool interval_passed;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+    struct arguments *args = state->input;
+
+    switch(key) {
+        case 'v':
+            ++args->verbose;
+            break;
+        case 'f':
+            args->hwmon = arg;
+            break;
+        case 'i':
+            if(!uint8_from_chars(arg, &args->interval)) {
+                args->interval = 0;
+            }
+            args->interval_passed = true;
+            break;
+        case 'c':
+            args->config = arg;
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+        }
     return 0;
 }
+
+static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
 
 int main(int argc, char** argv) {
-    char hwmon_path[HWMON_PATH_LEN] = { 0 };
-    char hwmon_subdir[HWMON_SUBDIR_LEN] = { 0 };
-    char config_hwmon[HWMON_PATH_LEN];
-    uint8_t config_interval = 5;
-    bool aggressive_throttle = false;
-    enum interpolation_method interp = linear;
+    char hwmon_full_path[HWMON_PATH_LEN];
+
+    /* For values specified in config */
+    char hwmon_buf[HWMON_PATH_LEN]; 
+    uint8_t config_interval = 0;
     uint8_t mtrx_rows;
+    enum interpolation_method interp = linear;
+    bool aggressive_throttle = false;
     matrix mtrx;
 
-    hwmon = hwmon_subdir;
+    struct arguments args = { 
+        .hwmon = 0, 
+        .config = FANCTL_CONFIG,
+        .verbose = 0, 
+        .interval = 5, 
+        .interval_passed = false
+    };
 
     /* Handle signals */
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
 
-    if(!compile_regexps()) {
+    argp_parse(&argp, argc, argv, 0, 0, &args);
+
+    set_log_level(args.verbose);
+
+    if(!parse_config(args.config, hwmon_buf, sizeof hwmon_buf, &config_interval, &aggressive_throttle, &interp, mtrx, &mtrx_rows)) {
         return 1;
     }
 
-    /* Find hwmon subdir */
-    if(!find_dir_matching_pattern(hwmon_subdir, sizeof hwmon_subdir, "^hwmon[0-9]$", HWMON_DIR)) {
-        fprintf(stderr, "No hwmon directory found in %s\n", HWMON_DIR);
-        return 1;
+    if(args.interval_passed) {
+        if(!args.interval) {
+            fprintf(stderr, "Update interval must be a number in 1...255\n");
+            return 1;
+        }
+    }
+    else if(config_interval) {
+        args.interval = config_interval;
     }
 
-    if(parse_arguments(argc, argv)) {
-        return 1;
-    }
-
-    if(!parse_config(config, config_hwmon, sizeof config_hwmon, &config_interval, &aggressive_throttle, &interp, mtrx, &mtrx_rows)) {
-        return 1;
-    }
-
-    /* hwmon not passed as argument and hwmon specified in config */
-    if(!hwmon_passed && strlen(config_hwmon) > 0) {
-        hwmon = config_hwmon;
-    }
-    /* Interval not passed as argument */
-    if(!interval_passed) {
-        update_interval = config_interval;
+    /* Set hwmon, precedence is argument -> config -> default */
+    if(!args.hwmon) {
+        if(!strlen(hwmon_buf)) {
+            if(!find_dir_matching_pattern(hwmon_buf, sizeof hwmon_buf, "^hwmon[0-9]$", HWMON_DIR)) {
+                fprintf(stderr, "No hwmon directory found in %s\n", HWMON_DIR);
+                return 1;
+            }
+        }
+        args.hwmon = hwmon_buf;
     }
 
     /* Verify hwmon subdir */
-    if(!is_valid_hwmon_dir(hwmon)) {
-        fprintf(stderr, "%s is not a valid hwmon directory\n", hwmon);
+    if(!is_valid_hwmon_dir(args.hwmon)) {
+        fprintf(stderr, "%s is not a valid hwmon directory\n", args.hwmon);
         return 1;
     }
 
-    /* Generate hwmon path */
-    if(strscat(hwmon_path, HWMON_DIR, sizeof hwmon_path) < 0) {
-        fprintf(stderr, "hwmon dir overflows the path buffer\n");
+    if(!generate_hwmon_path(hwmon_full_path, args.hwmon, sizeof hwmon_full_path)) {
         return 1;
     }
 
-    if(strscat(hwmon_path, hwmon, sizeof hwmon_path) < 0) {
-        fprintf(stderr, "%s would overflow the buffer when appended to %s\n", hwmon, hwmon_path);
-        return 1;
-    }
-
-    if(!amdgpu_daemon_init(config, hwmon_path, aggressive_throttle, interp, mtrx, mtrx_rows)) {
+    if(!amdgpu_daemon_init(args.config, hwmon_full_path, aggressive_throttle, interp, mtrx, mtrx_rows)) {
         fprintf(stderr, "Failed to initialize daemon\n");
         return 1;
     }
 
-    amdgpu_daemon_run(update_interval);
+    amdgpu_daemon_run(args.interval);
 
     return 0;
 }
