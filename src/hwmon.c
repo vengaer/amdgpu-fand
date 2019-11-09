454b0c3af6d2c33430a5fc4c0d973aa6209847f7
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-11-09 12:41:00 +0100

    Write persistent path not set in config, set it

--- src/hwmon.c
+++ src/hwmon.c
@@ -1,10 +1,11 @@
+#include "config.h"
 #include "filesystem.h"
 #include "hwmon.h"
 #include "logger.h"
 #include "strutils.h"
 
-bool generate_hwmon_dir(char *restrict dst, char const *restrict src, size_t count) {
-    if(src[0]) {
+bool generate_hwmon_dir(char *restrict dst, char const *restrict src, size_t count, char const *restrict config) {
+    if(*src) {
         LOG(VERBOSITY_LVL3, "Generating hwmon dir from %s supplied in config\n", src);
         if(strscpy(dst, src, count) < 0 || strscat(dst, "/", count) < 0) {
             fprintf(stderr, "Persistent path %s overflows the buffer\n", src);
@@ -20,13 +21,18 @@ bool generate_hwmon_dir(char *restrict dst, char const *restrict src, size_t cou
             fprintf(stderr, "hwmon dir overflows the path buffer\n");
             return false;
         }
+
+        LOG(VERBOSITY_LVL2, "Replacing empty persistent path in config with %s\n", dst);
+        if(!replace_persistent_path_value(config, dst)) {
+            LOG(VERBOSITY_LVL1, "Failed to replace empty persistent path in config\n");
+        }
     }
 
     return true;
 }
 
 bool generate_hwmon_path(char *restrict dst, char const *restrict hwmon, size_t count) {
-    if(strscat(dst, hwmon, count) < 0) {
+    if(strscat(dst, "/", count) < 0 || strscat(dst, hwmon, count) < 0) {
         fprintf(stderr, "%s would overflow the buffer when appended to %s\n", hwmon, dst);
         return false;
     }
--- src/hwmon.h
+++ src/hwmon.h
@@ -2,7 +2,7 @@
 #define HWMON_H
 
 #define HWMON_SYMLINK "/sys/class/drm/card0"
-#define DRM_SUBDIR "/device/hwmon/"
+#define DRM_SUBDIR "/device/hwmon"
 
 #define HWMON_PATH_LEN 128
 #define HWMON_SUBDIR_LEN 16
@@ -10,7 +10,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-bool generate_hwmon_dir(char *restrict dst, char const *restrict src, size_t count);
+bool generate_hwmon_dir(char *restrict dst, char const *restrict src, size_t count, char const *restrict config);
 bool generate_hwmon_path(char *restrict dst, char const *restrict src, size_t count);
 
 #endif
