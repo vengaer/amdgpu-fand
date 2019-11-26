f19dc8a150205e74a50729c74390b86ece0edd8f
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2019-11-26 17:10:09 +0100

    Print degree synbol in matrix if locale is utf8

--- src/common/matrix.c
+++ src/common/matrix.c
@@ -1,15 +1,24 @@
+#include "charset.h"
 #include "matrix.h"
 
 #include <stdio.h>
 
+#define TEMP_SUFFIX_SIZE 16
+
 void matrix_print(matrix mtrx, size_t rows) {
+    char temp_suffix[TEMP_SUFFIX_SIZE];
+
+    if(degrees_celcius(temp_suffix, sizeof temp_suffix) < 0) {
+        fprintf(stderr, "Temperature suffix overflows the buffer\n");
+    }
+
     printf(
         "*----------------------*\n"
         "|   temp    |  speed   |\n"
         "*----------------------*\n");
     for(size_t i = 0; i < rows; i++) {
         printf(
-        "|   %03u 'C  |   %03u%%   |\n"
-        "*----------------------*\n", mtrx[i][0], mtrx[i][1]);
+        "|   %03u%s   |   %03u%%   |\n"
+        "*----------------------*\n", mtrx[i][0], temp_suffix, mtrx[i][1]);
     }
 }
