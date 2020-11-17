10db599ef0efd00ef39f44565d55dc7c177d2741
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2020-11-17 18:02:05 +0100

    Remove old version

--- src/common/charset.h
+++
@@ -1,6 +0,0 @@
-#ifndef CHARSET_H
-#define CHARSET_H
-
-char const *deg_celcius_str();
-
-#endif
--- src/common/commontype.c
+++
@@ -1,3 +0,0 @@
-#include "commontype.h"
-
-char const *speed_interface_value[2] = { "tachometer", "daemon" };
