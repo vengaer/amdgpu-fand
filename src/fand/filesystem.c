c70d0520d82d8514a1d93bdb9f65b36aa34d2ca9
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2020-12-05 19:33:05 +0100

    Add file and path utilities

--- src/fand/filesystem.c
+++ src/fand/filesystem.c
@@ -1,10 +1,32 @@
 #include "filesystem.h"
+#include "strutils.h"
 
 #include <errno.h>
+#include <string.h>
 
 #include <sys/stat.h>
 
 bool fsys_dir_exists(char const *path) {
     struct stat sb;
     return stat(path, &sb) == 0 && S_ISDIR(sb.st_mode);
+}
+
+bool fsys_file_exists(char const *path) {
+    struct stat sb;
+    return stat(path, &sb) == 0 && S_ISREG(sb.st_mode);
+}
+
+ssize_t fsys_append(char *stem, char const *app, size_t bufsize) {
+
+    ssize_t pos = strlen(stem);
+    pos += strscpy(stem + pos, "/", bufsize - pos);
+    if(pos < 0) {
+        return pos;
+    }
+    pos += strscpy(stem + pos, app, bufsize - pos);
+    if(pos < 0) {
+        return pos;
+    }
+
+    return pos;
 }
--- src/fand/filesystem.h
+++ src/fand/filesystem.h
@@ -2,7 +2,12 @@
 #define FILESYSTEM_H
 
 #include <stdbool.h>
+#include <stddef.h>
+
+#include <sys/types.h>
 
 bool fsys_dir_exists(char const *path);
+bool fsys_file_exists(char const *path);
+ssize_t fsys_append(char *stem, char const *app, size_t bufsize);
 
 #endif /* FILESYSTEM_H */
