ad4d013adcf05e7814bad7dc8879010f10aa8aac
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2020-12-19 12:36:47 +0100

    Remove record queue

--- src/test/main.c
+++ src/test/main.c
@@ -1,5 +1,4 @@
 #include "interpolation_test.h"
-#include "record_queue_test.h"
 #include "sha1_test.h"
 #include "serialize_test.h"
 #include "test.h"
@@ -7,12 +6,6 @@
 int main(void) {
     section(sha1);
     run(test_sha1);
-
-    section(record_queue);
-    run(test_record_queue_push_pop);
-    run(test_record_queue_peek);
-    run(test_record_queue_flush);
-    run(test_record_queue_full);
 
     section(interpolation);
     run(test_lerp);
--- src/test/record_queue_test.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_RECORD_QUEUE_H
-#define TEST_RECORD_QUEUE_H
-
-void test_record_queue_push_pop(void);
-void test_record_queue_peek(void);
-void test_record_queue_flush(void);
-void test_record_queue_full(void);
-
-#endif /* TEST_RECORD_QUEUE_H */
